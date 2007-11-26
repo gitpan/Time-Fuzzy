6f2f05f504fca41f0a475bf887c9d05d33f8cbef
Jerome Quelin <jquelin@cpan.org>
2007-11-26 11:45:15 +0000

    Import of JQUELIN/Time-Fuzzy-0.36 from CPAN.
    gitpan-cpan-distribution: Time-Fuzzy
    gitpan-cpan-version:      0.36
    gitpan-cpan-path:         JQUELIN/Time-Fuzzy-0.36.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

--- lib/Time/Fuzzy.pm
+++ lib/Time/Fuzzy.pm
@@ -19,7 +19,7 @@ use base qw[ Exporter Class::Accessor::Fast ];
 our @EXPORT = qw[ fuzzy ];
 __PACKAGE__->mk_accessors( qw[ dt fuzziness ] );
 
-our $VERSION   = '0.35';
+our $VERSION   = '0.36';
 our $FUZZINESS = 'medium';
 
 #--
